6c3933ae586bd802d3eff543781fc5e7c0883392
Jeel Agrawal <64205413+Jeel-Agrawal@users.noreply.github.com>
2021-06-28 12:58:55 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Java Script and Reactjs</h1>
+      <p>This was an amazing bootcamp taken up by Shaurya Shinha.</p>
+    </div>
+  );
+}
+
+export default Note;
